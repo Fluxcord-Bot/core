c93eb5ede29d55b625fb8d57fd685509882843e3
Jb Carreon <jb@jbc.lol>
2026-02-21 16:45:02 +0800

    add config example

---
+++ config.example.ts
@@ -0,0 +1,25 @@
+const Config = {
+  // The path for the bot's data directory. Probably do not touch if you're using Docker.
+  DataFolderPath: "/data",
+
+  // The Discord bot token
+  DiscordBotToken: "DISCORD_BOT_TOKEN",
+
+  // The Discord client ID
+  DiscordClientId: "0000000000000000000",
+
+  // The Fluxer bot token
+  FluxerBotToken: "FLUXER_BOT_TOKEN",
+
+  // The Fluxer temporary emoji store community ID
+  FluxerTempEmojiGuildId: "0000000000000000000",
+
+  // The categories that the bot will log, remove any that you don't need.
+  // Categories are, FLUXER, DISCORD, DB, META
+  LoggingCategories: ['FLUXER', 'DISCORD', /*'DB',*/ 'META'],
+
+  // The prefix of the bot
+  BotPrefix: 'fc!'
+}
+
+export default Config;
