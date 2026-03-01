050a1d26a8aea6b242987ebbe77a2c04bc9d49fc
Nyx Tutt <git@nyxt.dev>
2026-03-01 10:56:48 -0600

    multiple forms of meowing
    Signed-off-by: Jb Carreon <jb@jbc.lol>

--- commands/meow.js
+++ commands/meow.js
@@ -6,12 +6,15 @@ import Package from "../package.json" with { type: "json" };
 /**
  * @type {import('../utils/CommandSchema.d.ts').CommandSchema}
  */
+
+const meow = ["mreow", "mrrp", "meow"]
+
 const command = {
   name: "meow",
   description: ":3",
   requireElevated: false,
   async run(params, message, discordClient, fluxerClient) {
-    message.reply("meow");
+    message.reply(meow[Math.floor(math.random() * meow.length)]);
   },
 };
 
