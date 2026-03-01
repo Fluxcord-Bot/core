1630390677ba1dd28c661ff7e8dfdcb17aab711b
nomaakip <nomaakip@noreply.localhost>
2026-03-01 22:22:15 +0100

    Update commands/waf.js

--- commands/waf.js
+++ commands/waf.js
@@ -6,12 +6,14 @@ import Package from "../package.json" with { type: "json" };
 /**
  * @type {import('../utils/CommandSchema.js').CommandSchema}
  */
+const waf = ["waf", "arf", "waff", "awaf"];
+
 const command = {
   name: "waf",
   description: ":3",
   requireElevated: false,
   async run(params, message, discordClient, fluxerClient) {
-    message.reply("waf");
+    message.reply(waf[Math.floor(Math.random() * waf.length)]);
   },
 };
 
