4767edfc001da3cb51cbe0d30114fe57e8b415c8
Jb Carreon <jb@jbc.lol>
2026-03-05 01:23:15 +0800

    math wrong :neofox_sob:

--- commands/meow.js
+++ commands/meow.js
@@ -1,20 +1,37 @@
-import { EmbedBuilder } from "@fluxerjs/core";
-import Config from "../config.js";
-import { ChannelMap } from "../db/index.js";
-import Package from "../package.json" with { type: "json" };
-
 /**
  * @type {import('../utils/CommandSchema.d.ts').CommandSchema}
  */
 
-const meow = ["mreow", "mrrp", "meow", "miao", "miaow", "prrt", "brrt", "meooow", "miaaaaow", "rawr", "purrr", "nyan", "miau", "miaou", "mjau", "myau", "niau", "MEOEW", "MEOW", "mrrm", "nya"]
+const meow = [
+  "mreow",
+  "mrrp",
+  "meow",
+  "miao",
+  "miaow",
+  "prrt",
+  "brrt",
+  "meooow",
+  "miaaaaow",
+  "rawr",
+  "purrr",
+  "nyan",
+  "miau",
+  "miaou",
+  "mjau",
+  "myau",
+  "niau",
+  "MEOEW",
+  "MEOW",
+  "mrrm",
+  "nya",
+];
 
 const command = {
   name: "meow",
   description: ":3",
   requireElevated: false,
   async run(params, message, discordClient, fluxerClient) {
-    message.reply(meow[Math.floor(math.random() * meow.length)]);
+    message.reply(meow[Math.floor(Math.random() * meow.length)]);
   },
 };
 
--- commands/waf.js
+++ commands/waf.js
@@ -1,8 +1,3 @@
-import { EmbedBuilder } from "@fluxerjs/core";
-import Config from "../config.js";
-import { ChannelMap } from "../db/index.js";
-import Package from "../package.json" with { type: "json" };
-
 /**
  * @type {import('../utils/CommandSchema.js').CommandSchema}
  */
