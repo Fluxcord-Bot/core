eda36840607bf096daf2e3d73439939e50c03e15
Jb Carreon <jb@jbc.lol>
2026-06-28 00:00:52 +0800

    switch to hooks

--- db/index.js
+++ db/index.js
@@ -10,15 +10,17 @@ const sequelize = new Sequelize({
   dialectModule: sqlite3,
   storage: Config.DataFolderPath + "/fluxcord.db",
   logging: (msg) => log("DB", msg),
-  pool: { max: 1 },
+  hooks: {
+    afterConnect: async (connection) => {
+      if (!DefaultConfig.DatabaseEncryptionToken) return;
+      await runRaw(connection, "PRAGMA cipher_compatibility = 4;");
+      await runRaw(
+        connection,
+        `PRAGMA key = '${Config.DatabaseEncryptionToken.replace(/'/g, "''")}';`,
+      );
+    },
+  },
 });
-
-if (DefaultConfig.DatabaseEncryptionToken) {
-  await sequelize.query("PRAGMA cipher_compatibility = 4;");
-  await sequelize.query(
-    `PRAGMA key = ${sequelize.escape(Config.DatabaseEncryptionToken)};`,
-  );
-}
 
 await sequelize.query("PRAGMA wal_checkpoint(TRUNCATE);");
 await sequelize.query("VACUUM;");
